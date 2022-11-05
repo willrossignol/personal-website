bd19239817ad2801059c42561a7f7280ec17d3cd
William Rossignol <112958205+willrossignol@users.noreply.github.com>
2022-11-05 12:52:29 -0300

    project card

---
+++ src/lib/ProjectCard.svelte
@@ -0,0 +1,23 @@
+<script>
+  import svelteLogo from "../assets/svelte.svg"
+
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<div class='project'>
+  <img src={svelteLogo} alt="Svelte icon">
+  <p>This is a project</p>
+</div>
+
+<style>
+  .project {
+    width: 10em;
+    padding-top: 1em;
+    background-color: #363636;
+    border: 0.1em solid black;
+    border-radius: 4px;
+  }
+</style>
